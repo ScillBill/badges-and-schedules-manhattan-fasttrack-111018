18c638dd14bb41fdf2aa49597f616e363be5fbf5
Priscilla Vazquez <scillamae922@gmail.com>
2018-11-11 20:37:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 speaker_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 speaker_names.each do |speaker_name|
-  puts "Hello, my name is #{speaker_name}"
+  puts "Hello, my name is #{speaker_name}."
   
     
 end
